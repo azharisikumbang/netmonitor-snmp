4cf427b5c7aeef7e86e1dfbebcb872f24fff5ad6
Kum <azharisaputrasikumbang@gmail.com>
2024-04-27 22:59:20 +0700

    add: redirect method for redirecting request

--- src/Core/Response.php
+++ src/Core/Response.php
@@ -6,9 +6,31 @@ class Response implements Contract\ResponseInterface
 {
     public function render(string $template, string $content, array $headers = []): void
     {
-        if (file_exists($template)) require_once $template;
-        else {
+        if (file_exists($template))
+            require_once $template;
+        else
+        {
             throw new \RuntimeException('Server Error: Template file for role not found.');
         }
+    }
+
+    public function redirect(string $to, mixed $message = null, bool $permanent = true): void
+    {
+        if ($message)
+            session()->add('temp', $message);
+        header('Location: ' . $to, true, $permanent ? 301 : 302);
+        exit();
+    }
+
+    public function redirectTo(string $to, mixed $message = null, bool $permanent = true): void
+    {
+        $this->redirect($to, $message, $permanent);
+    }
+
+    public function notFound(): void
+    {
+        http_response_code(404);
+        html_not_found();
+        exit();
     }
 }
