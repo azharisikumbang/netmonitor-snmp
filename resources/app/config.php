67c2530bbda09fa2cc86d69a0ee9dcae18926d27
Kum <azharisaputrasikumbang@gmail.com>
2024-04-27 23:02:36 +0700

    fix: application support configuration

--- resources/app/config.php
+++ resources/app/config.php
@@ -4,28 +4,17 @@ return [
     'app' => [
         'site_title' => 'SITE_TITLE',
         'base_dir' => __DIR__ . '/../../',
-        'site_url' => '',
-        'base_url' => '',
+        'site_url' => 'http://localhost/netmonitor-snmp/public/',
+        'base_url' => 'http://localhost/netmonitor-snmp/public/',
         'templates_dir' => __DIR__ . '/../../frontend/templates/',
         'pages_dir' => __DIR__ . '/../../frontend/pages/'
     ],
     'database' => [
         'host' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'DB_NAME',
+        'username' => 'netmon',
+        'password' => 'netmon',
+        'database' => 'netmon',
         'port' => 3306
     ],
-    'disk' => [],
-    'roles' => [
-        [
-            'name' => 'PUBLIC',
-            'value' => 'PUBLIC',
-            'template' => 'public'
-        ],[
-            'name' => 'SUPER_ADMIN',
-            'value' => 'SUPER_ADMIN',
-            'template' => 'super-admin'
-        ]
-    ]
+    'disk' => []
 ];
