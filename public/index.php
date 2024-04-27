5bb685e23815ffc724e6df4a7468805b3d91eb21
Kum <azharisaputrasikumbang@gmail.com>
2024-04-27 22:55:17 +0700

    include database feature for app

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\App;
+use App\Core\Database;
 use App\Core\Manager;
 use App\Core\Router;
 use App\Core\Request;
@@ -11,30 +12,43 @@ use App\Core\Session;
 use App\Core\Template;
 use App\Core\Configuration;
 
+$appServices = require_once __DIR__ . '/../resources/app/services.php';
+$container = new DI\Container($appServices);
+
 $appConfiguration = require_once __DIR__ . '/../resources/app/config.php';
+
 $config = new Configuration();
-$config->add('app', $appConfiguration);
+$config->add('app', $appConfiguration['app']);
+$config->add('db', $appConfiguration['database']); // TODO: implementasikan di DI Container
 
 $manager = new Manager();
 $manager->setConfiguration($config);
 
-$session = new Session();;
+$session = new Session();
 $manager->setSession($session);
 
-$router = new Router(new Request($_REQUEST, $manager->getSession()));
-$pagesDir = $config->get('app', 'app')['pages_dir'];
+$router = new Router(new Request($_REQUEST));
+$pagesDir = $config->get('app', 'pages_dir');
 $router->setPagesLocation($pagesDir);
 $manager->setRouter($router);
 
 $template = new Template();
-$templateDir = $config->get('app', 'app')['templates_dir'];
+$templateDir = $config->get('app', 'templates_dir');
 $template->setTemplatePath($templateDir);
 $manager->setTemplate($template);
 
 $response = new Response();
 $manager->setResponse($response);
 
-$app = new App($manager);
+$database = Database::createInstance(
+    $config->get('db', 'database'),
+    $config->get('db', 'username'),
+    $config->get('db', 'password')
+);
+$manager->setDatabase($database);
+
+$app = new App($manager, $container);
+$app->loadFunction('functions');
 $app->setEnvironment('development');
 $app->setTimeZone('Asia/Jakarta');
 $app->run();
