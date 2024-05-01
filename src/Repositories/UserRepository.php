08a9fa700eb727d005756170ee96e7a1485f00d2
Kum <azharisaputrasikumbang@gmail.com>
2024-05-01 14:29:47 +0700

    set new attributes to entity

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -23,10 +23,15 @@ class UserRepository extends BaseRepository
         $user = new User();
         $user->setId($rows['id']);
         $user->setName($rows['name']);
+        $user->setContact($rows['contact']);
+        $user->setAddress($rows['address']);
         $user->setUsername($rows['username']);
         $user->setPassword($rows['password'], true);
         $user->setRole(Role::from($rows['role']));
         $user->setCreatedAt(new DateTimeImmutable($rows['created_at']));
+        $user->setUpdatedAt(
+            $rows['updated_at'] ? new DateTimeImmutable($rows['updated_at']) : null
+        );
 
         return $user;
     }
